1325b429e60b2e1137ffac6ea0a31eb42386d2db
amousaid <amousaid@e2r13p5.1337.ma>
2024-10-23 14:11:14 +0100

    4

---
+++ script.js
@@ -0,0 +1,6 @@
+const elements = document.querySelectorAll('#pro-img');
+
+document.getElementById("btn-1").addEventListener("click", function()
+{
+		elements[0].style.border = "2px solid hsl(14, 86%, 42%)";
+});
